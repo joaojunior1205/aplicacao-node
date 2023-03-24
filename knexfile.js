6adea2fc21fb2063c535b2bb78f589420cc1400c
Joao Junior <joaojunior1205@gmail.com>
2023-03-23 22:13:08 -0300

    Revert "docker compose"
    This reverts commit ac7f1961

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,10 @@ module.exports = {
         password: password,
         port: 5432
     },
-
+    pool: {
+        min: 2,
+        max: 10
+    },
     migrations: {
         tableName: 'knex_migrations'
     }
