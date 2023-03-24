56465fd8eb8e6e21c0a0507ebb4dba91a53630e8
Joao Junior <joaojunior1205@gmail.com>
2023-03-23 22:38:14 -0300

    ajust

--- knexfile.js
+++ knexfile.js
@@ -13,10 +13,7 @@ module.exports = {
         password: password,
         port: 5432
     },
-    pool: {
-        min: 2,
-        max: 10
-    },
+
     migrations: {
         tableName: 'knex_migrations'
     }
