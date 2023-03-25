61b73131d8d0b8c2d2d1c22f3a73952d4f987c68
Joao Junior <joaojunior1205@gmail.com>
2023-03-25 12:33:01 -0300

    ajust

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,11 @@
 module.exports = app => {
    app.route('/')
-       .get(async (req, res) => {
+       .get(async (request, response) => {
+           return response.status(200);
          app.db('launch')
               .select('id', 'descricao', 'tipo', 'recorrencia', 'valor', 'data_vencimento', 'data_criacao', 'data_atualizacao')
-              .then(launch => res.status(200).json({...launch}))
-              .catch(err => res.status(500).send(err));
+              .then(launch => response.status(200).json({...launch}))
+              .catch(err => response.status(500).send(err));
        })
 
    app.route('/launch')
